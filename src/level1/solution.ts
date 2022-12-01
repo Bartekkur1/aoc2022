570ba2d953572d11a605102a077ae9dba9def895
Bartłomiej Kurpanik <bartekkur1@gmail.com>
2022-12-01 08:37:20 +0100

    level 1

--- src/level1/solution.ts
+++ src/level1/solution.ts
@@ -1,1 +1,31 @@
-console.log('hello world!');
+import { readAs } from 'aoc-util';
+
+const inventories = readAs<number[][]>({
+  path: './src/level1/input.txt',
+  parser: (input: string[]) => {
+    const inventories: number[][] = [];
+    let elfInventory: number[] = [];
+    for (const line of input) {
+      if (/\d+/.test(line)) {
+        elfInventory.push(Number(line));
+      } else {
+        inventories.push(Object.assign([], elfInventory));
+        elfInventory = [];
+      }
+    }
+    return inventories;
+  }
+});
+
+const sum = (input: number[]): number => input.reduce((prev, next) => { prev += next; return prev; }, 0)
+
+const kcalAgr = inventories.map(inventory => sum(inventory));
+
+// part 1
+const mostKcal = Math.max(...kcalAgr);
+console.log(`Most carried kcal is: ${mostKcal}`);
+
+// part 2
+const top3 = kcalAgr.sort((a, b) => b - a).slice(0, 3);
+const top3Sum = sum(top3);
+console.log(`Kcal carried by top 3 elves if: ${top3Sum}`);
