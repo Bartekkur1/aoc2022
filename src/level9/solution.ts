dda4e5227af09252cb0d874e744ee6bb6e7bee78
Bartłomiej Kurpanik <bartekkur1@gmail.com>
2022-12-12 00:41:48 +0100

    Level 9 improvements

--- src/level9/solution.ts
+++ src/level9/solution.ts
@@ -12,6 +12,14 @@ interface Move {
   steps: number;
 }
 
+interface LinePart {
+  id: number;
+  next?: LinePart;
+  position: Position;
+  prevPosition: Position;
+  prev?: LinePart;
+};
+
 const moves = readAs<Move[]>({
   path: './src/level9/input.txt',
   parser: lines => lines.map(line => {
@@ -20,9 +28,9 @@ const moves = readAs<Move[]>({
   })
 });
 
-export const isNextTo = (p1: Position, p2: Position) => {
-  const { x: x1, y: y1 } = p1;
-  const { x: x2, y: y2 } = p2;
+const isNextTo = (p1: LinePart, p2: LinePart) => {
+  const { x: x1, y: y1 } = p1.position;
+  const { x: x2, y: y2 } = p2.position;
   if (Math.abs(x1 - x2) > 1) {
     return false;
   } else if (Math.abs(y1 - y2) > 1) {
@@ -35,8 +43,11 @@ export const isNextTo = (p1: Position, p2: Position) => {
   return true;
 };
 
-export const executeMove = (position: Position, move: Move) => {
+const executeMove = (line: LinePart[], move: Move) => {
   const { direction } = move;
+  const head = line[0];
+  head.prevPosition = Object.assign({}, head.position)
+  const { position } = head;
   if (direction === 'U') {
     position.y += 1;
   } else if (direction === 'D') {
@@ -46,39 +57,62 @@ export const executeMove = (position: Position, move: Move) => {
   } else if (direction === 'L') {
     position.x -= 1;
   }
-  return Object.assign({}, position);
 };
 
-export const executeMoves = (moves: Move[]) => {
-  const headPosition: Position = { x: 0, y: 0 };
-  let previousHeadPosition: Position = { x: 0, y: 0 };
-  let tailPosition: Position = { x: 0, y: 0 };
-  let headMap: Position[] = [{ x: 0, y: 0 }];
-  const tailVisited = new Set();
+const moveTowards = (part: LinePart, target: LinePart) => {
+  const { position: { x: targetX, y: targetY } } = target;
+  const { position } = part;
+  if (targetX > (position.x + 1)) {
+    position.x++;
+  }
+  if (targetX < (position.x - 1)) {
+    position.x--;
+  }
+  if (targetY > (position.y - 1)) {
+    position.y++;
+  }
+  if (targetY < (position.y + 1)) {
+    position.y--;
+  }
+};
 
+const createLine = (parts: number): LinePart[] => {
+  const line: LinePart[] = [];
+  for (let i = 0; i < parts; i++) {
+    line[i] = {
+      id: i,
+      prevPosition: { x: 0, y: 0 },
+      position: { x: 0, y: 0 },
+      next: line[i - 1],
+    }
+  }
+  return line;
+};
+
+const executeMoves = (line: LinePart[], moves: Move[]) => {
+  const tailVisited = new Set();
   for (const move of moves) {
     const { steps } = move;
     for (let i = 0; i < steps; i++) {
-      previousHeadPosition = Object.assign({}, headPosition);
-      headMap.push(executeMove(headPosition, move));
-      if (!isNextTo(headPosition, tailPosition)) {
-        tailPosition = Object.assign({}, previousHeadPosition);
-        tailVisited.add(`${tailPosition.x},${tailPosition.y}`);
+      executeMove(line, move);
+      for (const element of line) {
+        if (element.id === 0) continue;
+        if (!isNextTo(element, element.next)) {
+          if (element.id === 1) {
+            element.position = Object.assign({}, element.next.prevPosition)
+          } else {
+            moveTowards(element, element.next);
+          }
+          if (element.id === line.length - 1) {
+            tailVisited.add(`${element.position.x},${element.position.y}`);
+          }
+        }
       }
     }
   }
-
   return tailVisited;
 };
 
-export const mapToSet = (map: Position[]) => {
-  const set = new Set<string>();
-  for (const position of map) {
-    const { x, y } = position;
-    set.add(`${x},${y}`);
-  }
-  return set;
-};
-
-const tailVisited = executeMoves(moves);
+const level1Line = createLine(2);
+const tailVisited = executeMoves(level1Line, moves);
 console.log(`Level 1: ${tailVisited.size}`);
